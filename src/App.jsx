f2022c1f65964d385e41006084bd5a767453f0f4
bharathreddy20031910 <bharathreddyv2003@gmail.com>
2025-07-09 00:15:01 +0530

    Changes in the Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Contact from './components/Contact'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
 <Header/>
+<Contact/>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  
 })
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#3b94f8',
+      },
+    },
+  },
+}
